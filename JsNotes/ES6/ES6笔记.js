0deb3875b92fbea1dce839ea29a0d7802f1cf69f
codesboy <565195693@qq.com>
2019-07-18 00:16:44 +0800

    update

---
+++ JsNotes/ES6/ES6笔记.js
@@ -0,0 +1,22 @@
+let map = new Map()
+map.set('', '')
+
+for(let [key,value] of map.entries()){ // map的键值对
+
+}
+
+map.has()
+map.get()
+
+arr.includes()
+str.includes()
+
+
+// &&是第一个条件为true才会执行第二个语句 if(true) ...
+
+delete 方法
+if else 后面的语句只有一句的时候可以省略{} 
+
+Object.keys(arr)
+
+instanceof Array
